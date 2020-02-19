ae3c8c0610497ec49942cb540d711db01bb4f23b
foofangjee <foofangjee@gmail.com>
2020-02-19 23:20:22 +0800

    load map

--- Assignment/Assignment/Stage1.js
+++ Assignment/Assignment/Stage1.js
@@ -1,13 +1,17 @@
 ﻿var stage1 = {preload:preload, create: create};
+var map;
+var layer;
 
 function preload() {
-    game.load.tilemap('stage1', 'Stage1.JSON', null, Phaser.Tilemap.TILED_JSON);
+    game.load.tilemap('map', 'resource/Level1/Stage1.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.image('Assets', 'resource/Level1/Assets.png');
 }
 
 function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
     game.stage.backgroundColor = "#34202B";
-    var map = game.add.tilemap("stage1");
-    layer = map.createLayer('Tile Layer 1');
+    map = game.add.tilemap('map');
+    map.addTilesetImage('Assets');
+    layer = map.createLayer('BaseLayer');
     layer.resizeWorld();
 }
--- Assignment/Assignment/resource/Level2/Assets.tsx
+++ Assignment/Assignment/resource/Level2/Assets.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.2" name="Assets" tilewidth="16" tileheight="16" tilecount="322" columns="23">
- <image source="Legacy-Dungeon_V0.2_vacaroxa/Final/Assets/Assets.png" width="368" height="224"/>
+ <image source="Assets.png" width="368" height="224"/>
  <tile id="15">
   <animation>
    <frame tileid="15" duration="200"/>
