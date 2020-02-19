6d15b561f8633299c3786594cdf8b52a2be7ca0e
foofangjee <foofangjee@gmail.com>
2020-02-19 18:27:50 +0800

    update

--- Assignment/Assignment/Menu.js
+++ Assignment/Assignment/Menu.js
@@ -1,7 +1,6 @@
 ﻿var menu = {preload: preload, create: create};
 
 function preload() {
-    game.load.image('button', 'resource/button.png');
     game.load.image('messageBox', 'resource/messageBox.png');
     game.load.image('closeButton', 'resource/closeButton.png');
     game.load.image('background', 'resource/background.png');
@@ -11,20 +10,25 @@ function create() {
     var bg = game.add.sprite(0, 0, 'background');
     var style = { font: "100px Arial", fill: "#000000", align: "center" };
     var title = game.add.text(game.world.centerX, 100, "Journey", style);
+    var newGameButton;
+    var continueButton;
+    var instructionButton;
+    var optionButton;
+    var quitButton;
     title.anchor.set(0.5, 0.5);
 
     bg.width = game.width;
     bg.height = game.height;
 
-    createButton(game.world.centerX, game.world.centerY, "New Game", function () {
+    newGameButton = createButton(game.world.centerX, game.world.centerY - 100, "New Game", function () {
         game.state.start("Stage1");
     });
 
-    createButton(game.world.centerX, game.world.centerY + 100, "Continue", function () {
+    continueButton = createButton(game.world.centerX, game.world.centerY, "Continue", function () {
         //game.state.start();
     });
 
-    createButton(game.world.centerX, game.world.centerY + 200, "How To Play", function () {
+    instructionButton = createButton(game.world.centerX, game.world.centerY + 100, "How To Play", function () {
         if (this.msgBox)
             this.msgBox.destroy();
 
@@ -42,6 +46,12 @@ function create() {
         var instruction7 = game.add.text(msgBox.x + 20, msgBox.y + 210, "X - Push", style);
         var instruction8 = game.add.text(msgBox.x + 20, msgBox.y + 240, "Z - Pull", style);
         var instruction9 = game.add.text(msgBox.x + 20, msgBox.y + 270, "P - Pause / Unpause", style);
+
+        newGameButton.inputEnabled = false;
+        continueButton.inputEnabled = false;
+        instructionButton.inputEnabled = false;
+        optionButton.inputEnabled = false;
+        quitButton.inputEnabled = false;
 
         box.width = 500;
         box.height = 500;
@@ -63,33 +73,41 @@ function create() {
 
         closeButton.x = msgBox.x + 445;
         closeButton.y = msgBox.y + 15;
+        instruction.x = msgBox.width / 2 - instruction.width / 2;
         msgBox.x = game.width / 2 - msgBox.width / 2;
         msgBox.y = game.height / 2 - msgBox.height / 2;
-        instruction.x = msgBox.width / 2 - instruction.width / 2;
-
+        
         closeButton.inputEnabled = true;
         closeButton.events.onInputDown.add(function () {
+            newGameButton.inputEnabled = true;
+            continueButton.inputEnabled = true;
+            instructionButton.inputEnabled = true;
+            optionButton.inputEnabled = true;
+            quitButton.inputEnabled = true;
             msgBox.destroy();
         }, this);
 
         this.msgBox = msgBox;
     });
 
-    createButton(game.world.centerX, game.world.centerY + 300, "Options", function () {
+    optionButton = createButton(game.world.centerX, game.world.centerY + 200, "Options", function () {
         //game.state.start();
     });
 
-    createButton(game.world.centerX, game.world.centerY + 300, "Exit", function () {
+    quitButton = createButton(game.world.centerX, game.world.centerY + 300, "Exit", function () {
        //exit the game
     });
 }
 
 function createButton(positionX, positionY, text, callback) {
-    var button = game.add.button(positionX, positionY, "button", callback, this);
+    var button = game.add.button(positionX, positionY, "", callback, this);
     var style = { font: "30px Arial", fill: "#000000", align: "center" };
     var button_text = game.add.text(button.x, button.y, text, style);
+
     button.anchor.set(0.5, 0.5);
     button.width = 200;
     button.height = 100;
     button_text.anchor.set(0.5, 0.5);
+
+    return button;
 }
