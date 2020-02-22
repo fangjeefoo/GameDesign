537ad671fc9984a69d2b52ee690cfd340eaae02a
foofangjee <foofangjee@gmail.com>
2020-02-23 05:01:15 +0800

    update

--- Assignment - Save&LoadGame/Assignment/Menu.js
+++ Assignment - Save&LoadGame/Assignment/Menu.js
@@ -7,6 +7,7 @@ var newGameButton;
 var continueButton;
 var scoreButton;
 var game;
+var close_text;
 
 function init(data) {
     game = data[0];
@@ -32,13 +33,39 @@ function create() {
 
     newGameButton = createButton(game.world.centerX, game.world.centerY, "New Game", function () {
         game.state.start('Stage1', true, true, game);
-    });
+    }, null);
 
     continueButton = createButton(game.world.centerX, game.world.centerY + 35, "Continue", function () {
-        console.log('check continue button');
         if (localStorage.getItem('SaveFile') !== null)
             loadGame();
-    });
+        else {
+            if (this.msgBox)
+                this.msgBox.destroy();
+
+            var style = { font: "25px Arial", fill: "#000000", align: "left" };
+            var msgBox = game.add.group();
+            var box = game.add.sprite(0, 0, "messageBox");
+            var instruction = game.add.text(game.world.centerX - 180, game.world.centerY - 100, "Game is not exist!", style);
+            var closeButton = createButton(game.world.centerX, game.world.centerY, "OK", function () {
+                enableButton(true);
+                close_text.destroy();
+                msgBox.destroy();
+            });
+            enableButton(false);
+
+            box.width = 430;
+            box.height = 250;
+
+            msgBox.add(box);
+            msgBox.add(instruction);
+            msgBox.add(closeButton);
+
+            msgBox.x = game.width / 2 - msgBox.width / 2;
+            msgBox.y = game.height / 2 - msgBox.height / 2;
+
+            this.msgBox = msgBox;
+        }
+    }, null);
 
     instructionButton = createButton(game.world.centerX, game.world.centerY + 70, "How To Play", function () {
         if (this.msgBox)
@@ -86,11 +113,11 @@ function create() {
         }, this);
 
         this.msgBox = msgBox;
-    });
+    }, null);
 
     scoreButton = createButton(game.world.centerX, game.world.centerY + 105, "Score Board", function () {
         game.state.start('ScoreBoard', true, true, [game]);
-    });
+    }, null);
 }
 
 function enableButton(boolean) {
@@ -100,36 +127,47 @@ function enableButton(boolean) {
     scoreButton.inputEnabled = boolean;
 }
 
-function createButton(positionX, positionY, text, callback) {
+function createButton(positionX, positionY, text, callback, msgBox) {
     var button = game.add.button(positionX, positionY, "", callback, this);
     var style = { font: "30px Arial", fill: "#000000", align: "center" };
-    var button_text = game.add.text(button.x, button.y, text, style);
+    var button_text;
 
+    if (text == "OK"){
+        close_text = game.add.text(button.x, button.y, text, style);
+        close_text.anchor.set(0.5, 0.5);
+    }        
+    else {
+        button_text = game.add.text(button.x, button.y, text, style);
+        button_text.anchor.set(0.5, 0.5);
+    }
+        
     button.anchor.set(0.5, 0.5);
     button.width = 200;
     button.height = 50;
-    button_text.anchor.set(0.5, 0.5);
-
+    
     button.onInputOver.add(function () {
-        button_text.setShadow(3, 3, 'rgba(0,0,0,0.5)', 5);
+        if (text == "OK")
+            close_text.setShadow(3, 3, 'rgba(0,0,0,0.5)', 5);
+        else
+            button_text.setShadow(3, 3, 'rgba(0,0,0,0.5)', 5);
     }, this);
 
     button.onInputOut.add(function () {
-        button_text.setShadow();
+        if (text == "OK")
+            close_text.setShadow();
+        else
+            button_text.setShadow();
     }, this);
 
     return button;
 }
 
 function loadGame() {
-    console.log('check load game');
     var file = JSON.parse(localStorage.getItem('SaveFile'));
-
     var level = file.level;
 
     if (level == 1) 
         game.state.start('Stage1', true, true, game, true, file.playerPosX, file.playerPosY, file.life, file.score);
-    
     else 
         game.state.start('Stage2', true, true, game, true, file.playerPosX, file.playerPosY, file.life, file.score);
 }
